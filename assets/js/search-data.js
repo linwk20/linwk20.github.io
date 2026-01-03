600919ef080830302aae34b40a9da29d4898fe1b
linwk20 <86647905+linwk20@users.noreply.github.com>
2026-01-03 05:02:37 +0000

    Deploying to gh-pages from @ linwk20/linwk20.github.io@be800b1c65cafec4ac51ee594d877504dd4e8d3c 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,7 +4,7 @@ const ninja = document.querySelector('ninja-keys');
 // add the home and posts menu items
 ninja.data = [{
     id: "nav-about-me",
-    title: "About Me🌟",
+    title: "About Me 🌟",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
