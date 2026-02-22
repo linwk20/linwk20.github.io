784cc34febfbac0909ab91e1456f41e63b50dfd7
linwk20 <86647905+linwk20@users.noreply.github.com>
2026-02-22 04:05:55 +0000

    Deploying to gh-pages from @ linwk20/linwk20.github.io@49b9d28c5c785be28f11dfc9358e1444b2be6ed3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,14 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-publications",
+  },{id: "nav-research",
+          title: "Research 🔬",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/research/";
+          },
+        },{id: "nav-publications",
           title: "Publications📝",
           description: "",
           section: "Navigation",
